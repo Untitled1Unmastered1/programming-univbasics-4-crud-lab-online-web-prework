f56812ba48ac5974117f46176482b8dff54eb8fa
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-03 18:15:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array("create_an_array", "olive")
-  create_an_array << "olive"
+  "create_an_array" << "olive"
 end
 
 def add_element_to_start_of_array(array, element)
