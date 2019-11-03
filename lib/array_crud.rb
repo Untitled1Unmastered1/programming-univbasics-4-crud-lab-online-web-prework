4188d340d261d4a21a2c2e92a75c90afeaed6684
GABRIEL PEREZ <gabriel1994.gp@gmail.com>
2019-11-03 18:17:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   ["allison", "almond", "eyes", "smile"]
 end
 
-def add_element_to_end_of_array("create_an_array", "olive")
+def add_element_to_end_of_array(Array, E)
   "create_an_array" << "olive"
 end
 
